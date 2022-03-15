0b151d2e437a973ab589d040fc7c5758999d8286
Plamen Ivanov <paco.iwanow@gmail.com>
2022-03-15 21:23:15 +0200

    first commit

---
+++ pages/_slug.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="container">
+    <h1>{{ planet.title }}</h1>
+    <h1>{{ planet.title }}</h1>
+    <h1>{{ planet.title }}</h1>
+    <img :src="planet.image" alt="" width="200" height="200" class="rounded" />
+    <div>
+      <p>Description: {{ planet.description }}</p>
+      <p v-if="planet.radius">Radius: {{ planet.radius }}</p>
+      <p v-if="planet.distanceFromSun">
+        Distance from the Sun: {{ planet.distanceFromSun }}
+      </p>
+    </div>
+    <nuxt-link type="button" class="btn btn-outline-light" to="/">
+      Planets
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ params }) {
+    const planet = await fetch(
+      `https:/api.nuxtjs.dev/planets/${params.slug}`
+    ).then((res) => res.json())
+    return { planet }
+  },
+}
+</script>
+
+<style>
+@import '../assets/style.css';
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template >
+  <div id="container">
+    <h1>Planets</h1>
+    <h1>Planets</h1>
+    <h1>Planets</h1>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mt-5 rounded">
+      <nuxt-link type="button" class="btn btn-outline-light m-sm-auto" to="/mercury">Mercury</nuxt-link>
+      <nuxt-link type="button" class="btn btn-outline-light m-sm-auto" to="/venus">Venus</nuxt-link>
+      <nuxt-link type="button" class="btn btn-outline-light m-sm-auto" to="/earth">Earth</nuxt-link>
+      <nuxt-link type="button" class="btn btn-outline-light m-sm-auto" to="/mars">Mars</nuxt-link>
+      <nuxt-link type="button" class="btn btn-outline-light m-sm-auto" to="/jupiter">Jupiter</nuxt-link>
+      <nuxt-link type="button" class="btn btn-outline-light m-sm-auto" to="/saturn">Saturn</nuxt-link>
+      <nuxt-link type="button" class="btn btn-outline-light m-sm-auto" to="/uranus">Uranus</nuxt-link>
+      <nuxt-link type="button" class="btn btn-outline-light m-sm-auto" to="/neptune">Neptune</nuxt-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style>
+@import '../assets/style.css';
+</style>
