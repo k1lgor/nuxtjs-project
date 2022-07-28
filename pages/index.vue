d896b199b6d708ecb48fb691a2dcc4eb8ef1cc63
Plamen Ivanov <paco.iwanow@gmail.com>
2022-07-28 22:21:20 +0300

    refactoring

--- pages/index.vue
+++ pages/index.vue
@@ -4,14 +4,61 @@
     <h1>Planets</h1>
     <h1>Planets</h1>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark mt-5 rounded">
-      <nuxt-link type="button" class="btn btn-outline-light m-sm-auto" to="/mercury">Mercury</nuxt-link>
-      <nuxt-link type="button" class="btn btn-outline-light m-sm-auto" to="/venus">Venus</nuxt-link>
-      <nuxt-link type="button" class="btn btn-outline-light m-sm-auto" to="/earth">Earth</nuxt-link>
-      <nuxt-link type="button" class="btn btn-outline-light m-sm-auto" to="/mars">Mars</nuxt-link>
-      <nuxt-link type="button" class="btn btn-outline-light m-sm-auto" to="/jupiter">Jupiter</nuxt-link>
-      <nuxt-link type="button" class="btn btn-outline-light m-sm-auto" to="/saturn">Saturn</nuxt-link>
-      <nuxt-link type="button" class="btn btn-outline-light m-sm-auto" to="/uranus">Uranus</nuxt-link>
-      <nuxt-link type="button" class="btn btn-outline-light m-sm-auto" to="/neptune">Neptune</nuxt-link>
+      <nuxt-link
+        type="button"
+        class="btn btn-outline-light m-sm-auto"
+        to="/mercury"
+      >
+        Mercury
+      </nuxt-link>
+      <nuxt-link
+        type="button"
+        class="btn btn-outline-light m-sm-auto"
+        to="/venus"
+      >
+        Venus
+      </nuxt-link>
+      <nuxt-link
+        type="button"
+        class="btn btn-outline-light m-sm-auto"
+        to="/earth"
+      >
+        Earth
+      </nuxt-link>
+      <nuxt-link
+        type="button"
+        class="btn btn-outline-light m-sm-auto"
+        to="/mars"
+      >
+        Mars
+      </nuxt-link>
+      <nuxt-link
+        type="button"
+        class="btn btn-outline-light m-sm-auto"
+        to="/jupiter"
+        >Jupiter</nuxt-link
+      >
+      <nuxt-link
+        type="button"
+        class="btn btn-outline-light m-sm-auto"
+        to="/saturn"
+      >
+        Saturn
+      </nuxt-link>
+      <nuxt-link
+        type="button"
+        class="btn btn-outline-light m-sm-auto"
+        to="/uranus"
+      >
+        Uranus
+      </nuxt-link>
+      <nuxt-link
+        type="button"
+        class="btn btn-outline-light m-sm-auto"
+        to="/neptune"
+      >
+        Neptune
+      </nuxt-link>
     </nav>
   </div>
 </template>
